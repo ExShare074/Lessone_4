cf271a7da853b3276913440360a6d2cf8a27bfbc
ExShare074 <168080573+ExShare074@users.noreply.github.com>
2024-05-09 12:47:04 +0300

    new code 1

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-print ("я конфликтую")
-for i in range(10):
-    print("слово")
-a, b = 10, 20
-
-if c > 10:
-    print("c > 10")
-elif c == 10:
-    print("c = 10")
-else:
-    print("c < 10")
-
+a = 5
